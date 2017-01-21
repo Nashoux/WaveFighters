1f2a2ce623e9a0d740fd373fd2372e8dc3873e81
Nashoux <lfournierquincy@aol.com>
2017-01-21 10:22:10 +0100

    initial Commit

---
+++ Assets/SlowThemAll.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowThemAll : MonoBehaviour {
+
+
+
+	List<GameObject> enteredGameObject = new List<GameObject>();
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider col){
+
+		if (!enteredGameObject.Contains(col.gameObject)) {
+			enteredGameObject.Add (col.gameObject);
+			col.gameObject.GetComponent<WaveProjectile>().speed = col.gameObject.GetComponent<WaveProjectile>().speed / 2;
+		}
+
+	}
+	void OnTriggerExit(Collider col){
+
+		if (enteredGameObject.Contains(col.gameObject)) {
+			enteredGameObject.Remove(col.gameObject);
+			col.gameObject.GetComponent<WaveProjectile>().speed = col.gameObject.GetComponent<WaveProjectile>().speed * 2;
+		}
+
+
+	}
+}
