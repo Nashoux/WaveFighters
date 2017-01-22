60f212511894e09df9a68b2dac3debd1ddac0e9f
huulong <n.huu.long@gmail.com>
2017-01-22 14:19:23 +0100

    [MENU] Instructions and Controls

--- Assets/Game Jam Template/Scripts/ShowPanels.cs
+++ Assets/Game Jam Template/Scripts/ShowPanels.cs
@@ -7,7 +7,9 @@ public class ShowPanels : MonoBehaviour {
 	public GameObject optionsTint;							//Store a reference to the Game Object OptionsTint 
 	public GameObject menuPanel;							//Store a reference to the Game Object MenuPanel 
 	public GameObject pausePanel;							//Store a reference to the Game Object PausePanel 
-
+	public GameObject instructionsScreen;					//Store a reference to the Game Object InstructionsScreen 
+	public GameObject controlsScreen;						//Store a reference to the Game Object ControlsScreen 
+	public GameObject stageSelectionScreen;					//Store a reference to the Game Object StageSelectionScreen 
 
 	//Call this function to activate and display the Options panel during the main menu
 	public void ShowOptionsPanel()
@@ -47,6 +49,34 @@ public class ShowPanels : MonoBehaviour {
 	{
 		pausePanel.SetActive (false);
 		optionsTint.SetActive(false);
+	}
 
+	// Show instructions
+	public void ShowInstructionsScreen()
+	{
+		menuPanel.SetActive (false);
+		instructionsScreen.SetActive (true);
+	}
+
+	// Switch from instructions to controls
+	public void ShowControlsScreen()
+	{
+		instructionsScreen.SetActive (false);
+		controlsScreen.SetActive(true);
+	}
+
+	// Switch from main menu or controls to stage selection
+	public void ShowStageSelectionScreen()
+	{
+		if (menuPanel.activeSelf) menuPanel.SetActive (false);
+		if (controlsScreen.activeSelf) controlsScreen.SetActive (false);
+		stageSelectionScreen.SetActive(true);
+	}
+
+	// Switch from stage selection to main menu
+	public void BackToMainMenu()
+	{
+		stageSelectionScreen.SetActive (false);
+		menuPanel.SetActive(true);
 	}
 }
--- Assets/Game Jam Template/Scripts/StartOptions.cs
+++ Assets/Game Jam Template/Scripts/StartOptions.cs
@@ -8,16 +8,17 @@ public class StartOptions : MonoBehaviour {
 
 
 
-	public int sceneToStart = 1;										//Index number in build settings of scene to load if changeScenes is true
+	int sceneToStart;										//Index number in build settings of scene to load if changeScenes is true
 	public bool changeScenes;											//If true, load a new scene when Start is pressed, if false, fade out UI and continue in single scene
 	public bool changeMusicOnStart;										//Choose whether to continue playing menu music or start a new music clip
 
 
-	[HideInInspector] public bool inMainMenu = true;					//If true, pause button disabled in main menu (Cancel in input manager, default escape key)
-	[HideInInspector] public Animator animColorFade; 					//Reference to animator which will fade to and from black when starting game.
-	[HideInInspector] public Animator animMenuAlpha;					//Reference to animator that will fade out alpha of MenuPanel canvas group
+	public bool inMainMenu = true;					//If true, pause button disabled in main menu (Cancel in input manager, default escape key)
+	public Animator animColorFade; 					//Reference to animator which will fade to and from black when starting game.
+//	[HideInInspector] public Animator animMenuAlpha;					//Reference to animator that will fade out alpha of MenuPanel canvas group
+	public Animator animStageSelectionScreenAlpha;	//Reference to animator that will fade out alpha of StageSelectionScreen canvas group
 	 public AnimationClip fadeColorAnimationClip;		//Animation clip fading to color (black default) when changing scenes
-	[HideInInspector] public AnimationClip fadeAlphaAnimationClip;		//Animation clip fading out UI elements alpha
+	public AnimationClip fadeAlphaAnimationClip;		//Animation clip fading out UI elements alpha
 
 
 	private PlayMusic playMusic;										//Reference to PlayMusic script
@@ -35,8 +36,10 @@ public class StartOptions : MonoBehaviour {
 	}
 
 
-	public void StartButtonClicked()
+	public void StartButtonClicked(int newSceneToStart)
 	{
+		sceneToStart = newSceneToStart;
+
 		//If changeMusicOnStart is true, fade out volume of music group of AudioMixer by calling FadeDown function of PlayMusic, using length of fadeColorAnimationClip as time. 
 		//To change fade time, change length of animation "FadeToColor"
 		if (changeMusicOnStart) 
@@ -115,11 +118,11 @@ public class StartOptions : MonoBehaviour {
 			Invoke ("PlayNewMusic", fadeAlphaAnimationClip.length);
 		}
 		//Set trigger for animator to start animation fading out Menu UI
-		animMenuAlpha.SetTrigger ("fade");
+//		animMenuAlpha.SetTrigger ("fade");
+		animStageSelectionScreenAlpha.SetTrigger ("fade");
 		Invoke("HideDelayed", fadeAlphaAnimationClip.length);
 		Debug.Log ("Game started in same scene! Put your game starting stuff here.");
 	}
-
 
 	public void PlayNewMusic()
 	{
